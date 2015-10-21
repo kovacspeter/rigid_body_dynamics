fc8652133371a63e5aad7cf9e50d4360a9cf72e8
Peter Kovacs <kovacs.pjotr@gmail.com>
2015-10-21 19:43:26 +0200

    Menu creation sample.

--- app/js/App.js
+++ app/js/App.js
@@ -2,24 +2,37 @@ function init(canvas, menu) {
   new App($('#' + canvas), $('#' + menu))
 };
 
-function App(canvas, menu) {
+function App(canvasElement, menu) {
   this.menu = menu;
-  this.canvas = canvas;
-  this.context = canvas[0].getContext("2d");
-  this.control = this.initControls();
-  console.log(canvas);
+  this.canvasElement = canvasElement;
+  this.canvas = new Canvas(canvasElement);
+  this.control = this.initControls(this.menu);
 };
 
-App.prototype.initControls = function() {
-  this.createButton(this.menu, 'lala', 'lala', function() {console.log('lala');})
+App.prototype.initControls = function(where) {
+
+  this.createButton(where, 'Create Sphere', 'createSphere', function() {
+
+    console.log(this);
+    this.canvasElement.click(function(evt) {
+      var x = evt.pageX - this.canvasElement.offset().left
+      var y = evt.pageY - this.canvasElement.offset().top
+      //TODO radius prompt by sa hodilo prerobit na nieco take ze bude tahat
+      //  kurzor po canvase a bude sa mu roztahova kruh od bodu kde klikol
+      this.canvas.createSphere(new Position(x, y, 0), prompt('Specify radius'));
+      this.canvasElement.unbind();
+    }.bind(this));
+
+  }.bind(this));
 };
 
 App.prototype.createButton = function(where, text, id, func){
   var button = $("<button/>", {
     id: id,
-    text: text
+    text: text,
+    click: func
   });
 
   where.append(button);
-  button.click(func);
+  return button;
 }
--- app/js/canvas.js
+++ app/js/canvas.js
@@ -1,6 +1,6 @@
-function drawCircle(context, x, y, radius, color) {
+function drawCircle(context, position, radius, color) {
   context.beginPath();
-  context.arc(x, y, radius, 0, 2*Math.PI);
+  context.arc(position.x, position.y, radius, 0, 2*Math.PI);
   context.fillStyle = color;
   context.fill();
   context.stroke();
@@ -14,3 +14,20 @@ function drawLine(context, fromPosition, toPosition, color, width) {
     context.lineTo(toPosition[0], toPosition[1]);
     context.stroke();
 }
+
+function Canvas(canvasElement) {
+  //state of canvas can be defined here, are we making sphere? applying force etc..
+  this.action = 0;
+  this.objects = [];
+  this.canvasElement = canvasElement;
+  this.context = this.canvasElement[0].getContext("2d");
+}
+
+Canvas.prototype.createSphere = function (position, radius) {
+  drawCircle(this.context, position, radius, 'black');
+  // body...
+};
+
+Canvas.prototype.moveObject = function (obj) {
+  // body...
+};
