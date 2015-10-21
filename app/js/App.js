217ee1cdc4e5703221d564890eb7765283093f42
Peter Kovacs <kovacs.pjotr@gmail.com>
2015-10-21 17:13:54 +0200

    create app button

--- app/js/App.js
+++ app/js/App.js
@@ -1,14 +1,26 @@
-function init(canvas) {
-  new App(document.getElementById(canvas))
+function init(canvas, menu) {
+  new App($('#' + canvas), $('#' + menu))
 };
 
-function App(canvas) {
+function App(canvas, menu) {
+  this.menu = menu;
   this.canvas = canvas;
-  this.context = canvas.getContext("2d");
+  this.context = canvas[0].getContext("2d");
   this.control = this.initControls();
-  console.log('hura');
+  console.log(canvas);
 };
 
 App.prototype.initControls = function() {
-  
+  this.createButton(this.menu, 'lala', 'lala', function() {console.log('lala');})
 };
+
+App.prototype.createButton = function(where, text, id, func){
+  var button = $("<button/>", {
+    id: id,
+    text: text
+  });
+
+  where.append(button);
+  where.on("click", "button#" + id, func);
+
+}
