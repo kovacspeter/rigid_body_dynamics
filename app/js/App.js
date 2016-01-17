d77a20ff900159e67fc6eab37c01fccf7e7f6d80
Jan Vodila <jan.vodila@binary-bay.com>
2016-01-17 17:36:10 +0100

    App variable as singleton

--- app/js/App.js
+++ app/js/App.js
@@ -1,54 +1,44 @@
-function init(canvasID, menuID, inspectorID) {
-	new App($('#' + canvasID), $('#' + menuID), $('#' + inspectorID));
-};
+var App = {
+	init: function (canvasID, menuID, inspectorID) {
+		App.menu = $('#' + menuID);
+		App.canvasElement = $('#' + canvasID);
+		App.canvas = new Canvas(App.canvasElement);
+		App.control = App.initControls(this.menu);
+		App.objectInspector = new Inspector($('#' + inspectorID));
+		App.run();
+	},
+	initControls: function (where) {
+		App.createButton(where, 'Add particle', 'plus', 'addParticle', function () {
+			App.canvas.state = Canvas.STATES.ADDING_PARTICLE;
+		});
 
-function App($canvasElement, $menu, $inspector) {
-	this.menu = $menu;
-	this.canvasElement = $canvasElement;
-	this.canvas = new Canvas($canvasElement);
-	this.control = this.initControls(this.menu);
-	this.objectInspector = new Inspector($inspector);
-	this.run();
-};
+		App.createButton(where, 'Apply Force', 'hand-right', 'applyForce', function () {
+			App.canvas.state = Canvas.STATES.APPLYING_FORCE;
+		});
+	},
+	createButton: function (where, text, icon, id, clickFunction) {
+		var button = $("<button/>", {
+			id: id,
+			html: '<i class="glyphicon glyphicon-' + icon + '"></i> ' + text,
+			click: function () {
+				clickFunction();
+				$(this).addClass('active');
+			}
+		});
 
-App.prototype.initControls = function (where) {
-
-	var states = Canvas.STATES;
-
-	this.createButton(where, 'Add particle', 'plus', 'addParticle', function () {
-		this.canvas.state = states.ADDING_PARTICLE;
-	}.bind(this));
-
-	this.createButton(where, 'Apply Force', 'hand-right', 'applyForce', function () {
-		this.canvas.state = states.APPLYING_FORCE;
-	}.bind(this));
-};
-
-App.prototype.createButton = function (where, text, icon, id, clickFunction) {
-	var button = $("<button/>", {
-		id: id,
-		html: '<i class="glyphicon glyphicon-' + icon + '"></i> ' + text,
-		click: function () {
-			clickFunction();
-			$(this).addClass('active');
+		where.append(button);
+		return button;
+	},
+	resetButtons: function() {
+		App.menu.find('button').removeClass('active');
+	},
+	run: function () {
+		if (App.canvas.state == null) {
+			App.canvas.update();
+			App.canvas.render();
 		}
-	});
+		App.objectInspector.refresh(App.canvas.objects);
 
-	where.append(button);
-	return button;
-};
-
-App.prototype.run = function () {
-	if (this.canvas.state == null) {
-		this.canvas.update();
-		this.canvas.render();
+		window.requestAnimationFrame(App.run);
 	}
-	this.objectInspector.refresh(this.canvas.objects);
-
-	window.requestAnimationFrame(this.run.bind(this));
-};
-
-/* GLOBAL METHODS */
-App.resetButtons = function () {
-	$('#menu').find('button').removeClass('active');
 };
